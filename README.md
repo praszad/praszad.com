3ae8ec05b27e65a69424df11e770eae7256faae9
Vinoth Kumar <vnoitkumar@gmail.com>
2020-01-26 00:47:09 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# [#vnoitkumar](https://www.google.com/search?q=%23vnoitkumar) - Vinoth Kumar
+
+Web Developer, skilled in all stages of the development life cycle for a dynamic web project. Proficient in Laravel, React, JavaScript, Docker, AWS and GCP.
---
+++ src/sw.js
@@ -0,0 +1,35 @@
+importScripts('./cache-polyfill.js');
+const version = '10.0.0';
+const cacheName = `vnoit-${version}`;
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache
+                .addAll([
+                    './',
+                    './images/avatar.png',
+                    './css/main.css',
+                    './css/all.css',
+                    './favicon.ico'
+                ])
+                .then(() => self.skipWaiting());
+        })
+    );
+});
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches
+            .open(cacheName)
+            .then(cache =>
+                cache.match(event.request, {
+                    ignoreSearch: true
+                })
+            )
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
