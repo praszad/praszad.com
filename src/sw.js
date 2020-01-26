15b3310bff2b73639617630e9c3ef00bfab2e047
Vinoth Kumar <vnoitkumar@gmail.com>
2020-01-26 09:12:45 +0530

    Prettier added and desc changed

--- src/sw.js
+++ src/sw.js
@@ -1,35 +1,36 @@
 importScripts('./cache-polyfill.js');
-const version = '10.0.0';
+const version = '11.0.0';
 const cacheName = `vnoit-${version}`;
 self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open(cacheName).then(cache => {
-            return cache
-                .addAll([
-                    './',
-                    './images/avatar.png',
-                    './css/main.css',
-                    './css/all.css',
-                    './favicon.ico'
-                ])
-                .then(() => self.skipWaiting());
-        })
-    );
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache
+        .addAll([
+          './',
+          './images/avatar.png',
+          './images/avatar.webp',
+          './css/main.css',
+          './css/all.css',
+          './favicon.ico'
+        ])
+        .then(() => self.skipWaiting());
+    })
+  );
 });
 self.addEventListener('activate', event => {
-    event.waitUntil(self.clients.claim());
+  event.waitUntil(self.clients.claim());
 });
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches
-            .open(cacheName)
-            .then(cache =>
-                cache.match(event.request, {
-                    ignoreSearch: true
-                })
-            )
-            .then(response => {
-                return response || fetch(event.request);
-            })
-    );
+  event.respondWith(
+    caches
+      .open(cacheName)
+      .then(cache =>
+        cache.match(event.request, {
+          ignoreSearch: true
+        })
+      )
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
